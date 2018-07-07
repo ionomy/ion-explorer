fa97136e0349d822af38da0176b0a4d1a800c827
LooorTor <LooorTor@gmail.com>
2018-07-07 12:22:27 +0800

    fix: fix wagerrd setup script

--- script/wagerrd_testnet_setup.sh
+++ script/wagerrd_testnet_setup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # Download latest node and install.
-wgrlink=`curl -s https://api.github.com/repos/wagerr/wagerr/releases/latest | grep browser_download_url | grep x86_64-linux | cut -d '"' -f 4`
+wgrlink=`curl -s https://api.github.com/repos/wagerr/wagerr/releases/latest | grep browser_download_url | grep x86_64-linux-gnu.tar.gz | cut -d '"' -f 4`
 mkdir -p /tmp/wagerr
 cd /tmp/wagerr
-curl -Lo wagerr.tar.gz $wgrlink
+wget -O wagerr.tar.gz $wgrlink
 tar -xvf wagerr.tar.gz
-wgrfolder = ls | grep wagerr-
-cd wgrfolder
+wgrfolder=`ls | grep wagerr-`
+cd /tmp/wagerr/$wgrfolder
 sudo mv ./bin/* /usr/local/bin
 cd
 rm -rf /tmp/wagerr
