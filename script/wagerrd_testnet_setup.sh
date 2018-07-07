e8878b420e66e699ac6a6105d975686c11a6b6ec
LooorTor <LooorTor@gmail.com>
2018-07-07 10:46:04 +0800

    feat: add wagerrd setup script

---
+++ script/wagerrd_testnet_setup.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# Download latest node and install.
+wgrlink=`curl -s https://api.github.com/repos/wagerr/wagerr/releases/latest | grep browser_download_url | grep x86_64-linux | cut -d '"' -f 4`
+mkdir -p /tmp/wagerr
+cd /tmp/wagerr
+curl -Lo wagerr.tar.gz $wgrlink
+tar -xvf wagerr.tar.gz
+wgrfolder = ls | grep wagerr-
+cd wgrfolder
+sudo mv ./bin/* /usr/local/bin
+cd
+rm -rf /tmp/wagerr
+mkdir /root/.wagerr
+
+# Setup configuration for node.
+rpcuser=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 13 ; echo '')
+rpcpassword=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 32 ; echo '')
+cat > /root/.wagerr/wagerr.conf <<EOL
+rpcuser=wagerr
+rpcpassword=this
+daemon=1
+txindex=1
+testnet=1
+enablezeromint=0
+staking=0
+addnode=35.189.104.248
+addnode=35.237.135.236
+addnode=35.237.49.54
+addnode=35.227.74.136
+EOL
+
+# Start node.
+wagerrd
