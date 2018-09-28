3c0965349a815ec0b4ef190486f0f246e87a4803
LooorTor <LooorTor@gmail.com>
2018-09-28 21:10:41 +0800

    feat: update config for obsr

--- config.js
+++ config.js
@@ -4,29 +4,29 @@
  */
 const config = {
   'api': {
-    'host': 'https://explorer.slate.io',
+    'host': 'https://explorer.obsr.xyz',
     'port': '8087',
     'prefix': '/api',
     'timeout': '10s'
   },
   'coinMarketCap': {
     'api': 'http://api.coinmarketcap.com/v1/ticker/',
-    'ticker': 'slate'
+    'ticker': 'obsr'
   },
   'db': {
     'host': '127.0.0.1',
     'port': '27017',
-    'name': 'slatex',
-    'user': 'slate',
-    'pass': 'slate'
+    'name': 'obsrx',
+    'user': 'obsr',
+    'pass': 'obsr'
   },
   'freegeoip': {
     'api': 'https://extreme-ip-lookup.com/json/'
   },
   'rpc': {
     'host': '127.0.0.1',
-    'port': '37414',
-    'user': 'slate',
+    'port': '9568',
+    'user': 'obsr',
     'pass': 'this',
     'timeout': 8000, // 8 seconds
   }
