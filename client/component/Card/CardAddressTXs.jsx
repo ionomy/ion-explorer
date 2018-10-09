c316961c189afecd6ba1bba9c0271ad7c949dc3f
LooorTor <LooorTor@gmail.com>
2018-10-09 08:03:43 +0800

    feat: remove unrelated things

--- client/component/Card/CardAddressTXs.jsx
+++ client/component/Card/CardAddressTXs.jsx
@@ -55,7 +55,7 @@ export default class CardAddressTXs extends Component {
             amount: (
               <span
                 className={ `badge badge-${ isSpent ? 'danger' : 'success' }` }>
-                { numeral(amount).format('0,0.0000') } WGR
+                { numeral(amount).format('0,0.0000') } OBSR
               </span>
             ),
             createdAt: date24Format(tx.createdAt),
